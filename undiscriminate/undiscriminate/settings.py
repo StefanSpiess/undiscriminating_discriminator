2a65004d449645f004233f47f7c50d9e1ebbf69a
Stefan Spiess <stefan.spiess.regensburg@gmail.com>
2025-06-12 15:09:14 +0200

    Clarify DEBUG ordering

--- undiscriminate/undiscriminate/settings.py
+++ undiscriminate/undiscriminate/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+# DEBUG must be defined before checking SECRET_KEY
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
